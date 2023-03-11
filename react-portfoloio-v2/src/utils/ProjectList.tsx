92990c5eac6bffbc13dc95741ddfcf65f0958766
Salih Alperen <sal.alperen1@gmail.com>
2023-03-11 15:11:47 +0100

    Added real photos

--- react-portfoloio-v2/src/utils/ProjectList.tsx
+++ react-portfoloio-v2/src/utils/ProjectList.tsx
@@ -20,7 +20,7 @@ export default [
   {
     id: 3,
     image:
-      'https://salihalperen.netlify.app/static/media/takeAll.dc98399b23b12991a2fd.PNG',
+      'https://salihalperen.netlify.app/static/media/github.addf861248884c2540ae.PNG',
     techs: ['React', 'Auth0', 'Styled Components'],
     title: 'Github User',
     live: 'https://github-user-search-app-react.netlify.app/',
@@ -29,9 +29,9 @@ export default [
   {
     id: 4,
     image:
-      'https://salihalperen.netlify.app/static/media/takeAll.dc98399b23b12991a2fd.PNG',
+      'https://salihalperen.netlify.app/static/media/todo.d5c5b0bb416c1cb62229.png',
     techs: ['React', 'Auth0', 'Scss'],
-    title: 'Github User',
+    title: 'Todo',
     live: 'https://react-todo-app-challenge.netlify.app/',
     repo: 'https://github.com/salperen-jpg/react-todo-app-redux',
   },
