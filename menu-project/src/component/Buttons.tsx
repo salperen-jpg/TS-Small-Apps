9c1ca58153e255cd722ffe34916b9c29b656f39b
Salih Alperen <sal.alperen1@gmail.com>
2023-03-19 22:13:22 +0100

    added active btn functionality

--- menu-project/src/component/Buttons.tsx
+++ menu-project/src/component/Buttons.tsx
@@ -3,15 +3,24 @@ import React from 'react';
 interface ButtonTypes {
   categories: string[];
   handleFood: (c: string) => void;
+  currentCat: string;
 }
 
-const Buttons: React.FC<ButtonTypes> = ({ categories, handleFood }) => {
+const Buttons: React.FC<ButtonTypes> = ({
+  categories,
+  handleFood,
+  currentCat,
+}) => {
   return (
     <section>
       <div className='btn-container'>
         {categories.map((m, index) => {
           return (
-            <button key={index} className='btn' onClick={() => handleFood(m)}>
+            <button
+              key={index}
+              className={`${currentCat === m ? 'btn active' : 'btn'}`}
+              onClick={() => handleFood(m)}
+            >
               {m}
             </button>
           );
--- menu-project/src/component/MenuContainer.tsx
+++ menu-project/src/component/MenuContainer.tsx
@@ -7,16 +7,22 @@ const getCategories = ['all', ...new Set(menu.map((m) => m.category))];
 
 const MenuContainer = () => {
   const [food, setFood] = React.useState(menu);
+  const [currentCat, setCurrentCat] = React.useState('all');
 
   const handleFood = (category: string) => {
     category === 'all'
       ? setFood(menu)
       : setFood(menu.filter((m) => m.category === category));
+    setCurrentCat(category);
   };
 
   return (
     <>
-      <Buttons categories={getCategories} handleFood={handleFood} />
+      <Buttons
+        categories={getCategories}
+        handleFood={handleFood}
+        currentCat={currentCat}
+      />
       <Menu food={food} />
     </>
   );
