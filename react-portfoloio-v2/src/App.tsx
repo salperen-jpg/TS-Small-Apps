69d3a4d8932093775af9e1ae36d28e42f10df623
Salih Alperen <sal.alperen1@gmail.com>
2023-03-20 22:30:41 +0100

    added additional project comp

--- react-portfoloio-v2/src/App.tsx
+++ react-portfoloio-v2/src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
+import Projects2 from './components/Projects2';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Experience />
       <About />
       <Projects />
+      <Projects2 />
       <Footer />
     </main>
   );
--- react-portfoloio-v2/src/components/Projects2.tsx
+++ react-portfoloio-v2/src/components/Projects2.tsx
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
-import Title from './Title';
 import projectList from '../utils/ProjectList';
-const Projects = () => {
+import Title from './Title';
+
+const Projects2 = () => {
   return (
     <Wrapper id='Projects'>
       <Title title='Projects' />
       <div className='section-center project-list'>
         {projectList.map((project) => {
-          const { id, title, techs, image } = project;
+          const { id, title, techs, image, exp, live, repo } = project;
           return (
-            <article key={id} className={`project project-${id}`}>
+            <article key={id} className='project'>
               <img src={image} className='project-img' alt={title} />
               <div className='info'>
                 <h4>{title}</h4>
+                {exp}
                 <div className='techs'>
                   {techs.map((t, index) => {
                     return (
@@ -22,6 +24,11 @@ const Projects = () => {
                     );
                   })}
                 </div>
+              </div>
+              <div className='btn-container'>
+                <a href={live} className='btn live-btn'>
+                  live
+                </a>
               </div>
             </article>
           );
@@ -41,71 +48,86 @@ const Projects = () => {
 };
 
 const Wrapper = styled.section`
-  .section-center {
+  .project-list {
     width: var(--min-width);
     max-width: var(--max-width);
     margin: 0 auto;
     display: grid;
-    gap: 3rem;
+    gap: 7rem 13rem;
   }
   .project {
     position: relative;
+    border-radius: var(--radius);
     background: var(--clr-primary-500);
   }
-  .info {
-    text-align: center;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    opacity: 0;
-    transition: all 0.3s linear;
-    z-index: 1;
-    h4 {
-      color: var(--white);
-      letter-spacing: var(--spacing);
-      margin-bottom: 0.5rem;
-    }
-  }
-  .project:hover .info {
-    opacity: 1;
-  }
-  .project-img {
-    transition: all 0.3s linear;
-    max-height: 17rem;
-    object-fit: cover;
-  }
-  .project:hover .project-img {
-    opacity: 0.1;
-  }
-  .project::after {
+  .project::after,
+  .project::before {
     content: '';
     position: absolute;
-    inset: 0;
-    border: 5px solid transparent;
-  }
-  .project:hover::after {
-    border-image-source: linear-gradient(
-      to right,
+    inset: -0.5rem;
+    z-index: -1;
+    background: conic-gradient(
+      from var(--gradient-angle),
       var(--blueish),
       var(--pinkish)
     );
-    border-image-slice: 3;
-    transform: scale(0.8);
+    border-radius: inherit;
+    animation: rotation 20s linear infinite;
   }
+  .card::after {
+    filter: blur(5rem);
+  }
+  @property --gradient-angle {
+    syntax: '<angle>';
+    initial-value: 0deg;
+    inherits: false;
+  }
+  @keyframes rotation {
+    0% {
+      --gradient-angle: 0deg;
+    }
+    100% {
+      --gradient-angle: 360deg;
+    }
+  }
+  .project img {
+    height: 17rem;
+    object-fit: cover;
+    border-top-left-radius: var(--radius);
+    border-top-right-radius: var(--radius);
+  }
+  .info {
+    padding: 1rem 0.5rem;
+    h4 {
+      margin-bottom: 1rem;
+      color: var(--white);
+    }
+    p {
+      line-height: 1.75;
+      margin-bottom: 1rem;
+    }
+    .techs {
+      display: flex;
 
-  .techs {
+      flex-wrap: wrap;
+      gap: 0.5rem;
+      margin-top: 1rem;
+    }
+    .tech {
+      background: var(--grey-100);
+      padding: 0.25rem 0.3rem;
+      border-radius: var(--radius);
+      color: var(--grey-900);
+    }
+  }
+  .btn-container {
+    padding: 1rem 0.5rem;
     display: flex;
     justify-content: center;
-    flex-wrap: wrap;
-    gap: 0.5rem;
-    margin-top: 1rem;
-  }
-  .tech {
-    background: var(--grey-100);
-    padding: 0.25rem 0.3rem;
-    border-radius: var(--radius);
-    color: var(--grey-900);
+    gap: 1rem;
+    .live-btn {
+      background: linear-gradient(to right, var(--blueish), var(--pinkish));
+    }
   }
   .projects-link {
     text-align: center;
@@ -123,40 +145,12 @@ const Wrapper = styled.section`
   .projects-btn:hover {
     background: linear-gradient(to right, var(--blueish), var(--pinkish));
   }
-  @media (min-width: 650px) {
-    .section-center {
-      grid-template-columns: repeat(2, 1fr);
-    }
-  }
 
-  @media (min-width: 992px) {
-    .section-center {
-      grid-template-columns: repeat(3, 1fr);
-      grid-template-rows: 200px 200px;
-      grid-template-areas:
-        'a b b'
-        'a c d';
-    }
-    .project-1 {
-      grid-area: a;
-    }
-    .project-2 {
-      grid-area: b;
-    }
-    .project-3 {
-      grid-area: c;
-    }
-    .project-4 {
-      grid-area: d;
-    }
-    .project {
-      height: 100%;
-    }
-    .project-img {
-      max-height: max-content;
-      height: 100%;
+  @media (min-width: 800px) {
+    .project-list {
+      grid-template-columns: repeat(2, 1fr);
     }
   }
 `;
 
-export default Projects;
+export default Projects2;
