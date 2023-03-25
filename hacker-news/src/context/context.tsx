64e525af97eb17993bfb5f0aaaea81f62b9c2b71
Salih Alperen <sal.alperen1@gmail.com>
2023-03-25 23:10:54 +0100

    Functionality of app

--- hacker-news/src/context/context.tsx
+++ hacker-news/src/context/context.tsx
@@ -2,22 +2,25 @@ import { useContext, createContext, useReducer, useEffect } from 'react';
 import { reducer } from '../reducer/reducer';
 import { API_ENDPOINT } from '../utils/constants';
 import axios from 'axios';
-interface NewContextType {}
+interface NewContextType {
+  hits: [];
+  query: string;
+  handleQuery: (v: string) => void;
+  isLoading: boolean;
+  page: number;
+  numberOfPages: number;
+  handlePage: (a: string) => void;
+}
 
 interface Children {
   children: React.ReactNode;
 }
 
-// export interface StateType {
-//   query: string;
-//   hits: [];
-//   isLoading: boolean;
-//   page: number;
-// }
-
 export const enum REDUCER_ACTION_TYPE {
-  SET_LOADING,
-  SET_NEWS,
+  SET_LOADING = 'SET_LOADING',
+  SET_NEWS = 'SET_NEWS',
+  SET_QUERY = 'SET_QUERY',
+  HANDLE_PAGE = 'HANDLE_PAGE',
 }
 
 export interface ActionTypes {
@@ -31,31 +34,56 @@ export const initialState = {
   numberOfHits: 0,
   isLoading: false,
   page: 0,
+  numberOfPages: 0,
 };
 
-const NewsContext = createContext<NewContextType>({});
+const NewsContext = createContext<NewContextType>({
+  hits: [],
+  query: '',
+  isLoading: false,
+  page: 0,
+  numberOfPages: 0,
+  handleQuery: () => {},
+  handlePage: () => {},
+});
 
 export const AppProvider = ({ children }: Children) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const fetchStories = async () => {
+  const fetchStories = async (query: string) => {
     dispatch({ type: REDUCER_ACTION_TYPE.SET_LOADING });
     try {
       const { data } = await axios(
-        `${API_ENDPOINT}search?query=${state.query}`
+        `${API_ENDPOINT}search?query=${query}&page=${state.page}`
       );
       const { hits, nbHits, nbPages } = data;
-      dispatch();
+      dispatch({
+        type: REDUCER_ACTION_TYPE.SET_NEWS,
+        payload: { hits, nbHits, nbPages },
+      });
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    fetchStories();
-  }, []);
+  const handleQuery = (val: string) => {
+    dispatch({ type: REDUCER_ACTION_TYPE.SET_QUERY, payload: { val } });
+  };
 
-  return <NewsContext.Provider value='hi'>{children}</NewsContext.Provider>;
+  const handlePage = (val: string) => {
+    console.log(val);
+    dispatch({ type: REDUCER_ACTION_TYPE.HANDLE_PAGE, payload: { val } });
+  };
+
+  useEffect(() => {
+    fetchStories(state.query);
+  }, [state.query, state.page]);
+
+  return (
+    <NewsContext.Provider value={{ ...state, handleQuery, handlePage }}>
+      {children}
+    </NewsContext.Provider>
+  );
 };
 
 export const useNewsContext = () => useContext(NewsContext);
--- hacker-news/src/reducer/reducer.ts
+++ hacker-news/src/reducer/reducer.ts
@@ -6,4 +6,39 @@ export const reducer = (state: typeof initialState, action: ActionTypes) => {
   if (action.type === REDUCER_ACTION_TYPE.SET_LOADING) {
     return { ...state, isLoading: true };
   }
+  if (action.type === REDUCER_ACTION_TYPE.SET_NEWS) {
+    const { hits, nbHits, nbPages } = action.payload;
+    return {
+      ...state,
+      hits,
+      numberOfHits: nbHits,
+      numberOfPages: nbPages,
+      isLoading: false,
+    };
+  }
+  if (action.type === REDUCER_ACTION_TYPE.SET_QUERY) {
+    const { val } = action.payload;
+    return { ...state, query: val };
+  }
+  if (action.type === REDUCER_ACTION_TYPE.HANDLE_PAGE) {
+    const { val } = action.payload;
+    console.log(val);
+    const { page } = state;
+    let newPage;
+    if (val === 'inc') {
+      newPage = page + 1;
+      if (newPage > state.numberOfPages - 1) {
+        newPage = 0;
+      }
+    }
+    if (val === 'dec') {
+      newPage = page - 1;
+      if (newPage < 0) {
+        newPage = state.numberOfPages - 1;
+      }
+    }
+    console.log(newPage);
+    return { ...state, page: newPage };
+  }
+  throw new Error(`The action type (${action.type})  is not found`);
 };
