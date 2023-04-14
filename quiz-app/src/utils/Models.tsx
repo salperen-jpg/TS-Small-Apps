e96d44616d62c93f034b93568cc5583b316ee19c
Salih Alperen <sal.alperen1@gmail.com>
2023-04-14 20:38:53 +0200

    changed model

--- quiz-app/src/utils/Models.tsx
+++ quiz-app/src/utils/Models.tsx
@@ -6,26 +6,3 @@ export interface Question {
   correct_answer: string;
   incorrect_answers: string[];
 }
-
-export interface ContextProps {
-  isLoading: boolean;
-  isFormShown: boolean;
-  questions: Question[];
-  isError: boolean;
-  index: number;
-  correct: number;
-  nextQuestion: () => void;
-  checkAnswer: (a: boolean) => void;
-  closeModal: () => void;
-  isModalOpen: boolean;
-  quiz: {
-    amount: string;
-    category: string;
-    difficulty: string;
-  };
-  handleChange: (
-    e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
-  ) => void;
-  handleSubmit: (e: React.FormEvent) => void;
-  fetchQuestions: (url: string) => void;
-}
