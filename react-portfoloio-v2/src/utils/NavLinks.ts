3cb25b98fefe46e4c5c02d2a9929c2c18e36c26f
Salih Alperen <sal.alperen1@gmail.com>
2023-05-17 22:25:10 +0200

    Portfolio removed

--- react-portfoloio-v2/src/utils/NavLinks.ts
+++
@@ -1,5 +0,0 @@
-export const NavLinks = [
-  { id: 1, text: 'Projects', link: '#Projects' },
-  { id: 2, text: 'About', link: '#About' },
-  { id: 3, text: 'Experince', link: '#Experience' },
-];
--- react-portfoloio-v2/src/utils/Techs.ts
+++
@@ -1,8 +0,0 @@
-export default [
-  { id: 1, text: 'TypeScript' },
-  { id: 2, text: 'JavaScript' },
-  { id: 3, text: 'Reactjs' },
-  { id: 4, text: 'Expressjs / MongoDB' },
-  { id: 5, text: 'CSS/SCSS' },
-  { id: 6, text: 'HTML' },
-];
