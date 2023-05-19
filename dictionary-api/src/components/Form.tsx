8fa680a190727c4f70d5332521fcd656afdf0031
Salih Alperen <sal.alperen1@gmail.com>
2023-05-19 15:54:36 +0200

    changed icon and styled component

--- dictionary-api/src/components/Form.tsx
+++ dictionary-api/src/components/Form.tsx
@@ -1,26 +1,79 @@
-import { BsSearch } from 'react-icons/bs';
-import { useDictionaryApp } from '../context';
+import { FaSearch } from "react-icons/fa";
+import { useDictionaryApp } from "../context";
+import { styled } from "styled-components";
 const Form = () => {
   const { fetchDefinition } = useDictionaryApp();
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+    console.log("clicked");
+
     const inputVal = event.target!.elements!.word.value;
     if (!inputVal) return;
     fetchDefinition(inputVal);
   };
 
   return (
-    <section>
+    <Wrapper>
       <form className='section-center' onSubmit={handleSubmit}>
         <div className='form-container'>
-          <input type='text' name='word' className='form-input' />
+          <input
+            type='text'
+            name='word'
+            className='form-input'
+            placeholder='search for keyword...'
+          />
           <button type='submit' className='btn submit-btn'>
-            <BsSearch />
+            <FaSearch />
           </button>
         </div>
       </form>
-    </section>
+    </Wrapper>
   );
 };
 export default Form;
+
+const Wrapper = styled.section`
+  .form-container {
+    position: relative;
+    height: 4rem;
+  }
+
+  .form-input {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 4rem;
+    padding: 0.5rem 1rem;
+    border-radius: var(--borderRadius);
+    font-family: inherit;
+    letter-spacing: 1.5px;
+    font-size: 1rem;
+    font-weight: 700;
+    border: transparent;
+    outline: none;
+    background-color: var(--grey-100);
+  }
+  .form-input:focus {
+    border: 2px solid var(--primary-900);
+  }
+  .form-input::placeholder {
+    text-transform: capitalize;
+  }
+
+  .submit-btn {
+    position: absolute;
+    padding: 0.5rem 1rem;
+    display: block;
+    height: 100%;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    background-color: transparent;
+
+    color: var(--primary-900);
+    svg {
+      font-size: 1.5rem;
+    }
+  }
+`;
