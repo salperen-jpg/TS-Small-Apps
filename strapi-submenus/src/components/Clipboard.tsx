a8b7d1af5505f41e530ccbb523c39459484fd680
Salih Alperen <sal.alperen1@gmail.com>
2023-05-20 23:06:18 +0200

    clipboard functionality

--- strapi-submenus/src/components/Clipboard.tsx
+++ strapi-submenus/src/components/Clipboard.tsx
@@ -1,11 +1,30 @@
-import { HiOutlineClipboard } from 'react-icons/hi';
+import { useState, useEffect } from "react";
+import { HiOutlineClipboard } from "react-icons/hi";
+import { TiTick } from "react-icons/ti";
+let text = "npx create-strapi-app@latest my-project ";
+
 const Clipboard = () => {
+  const [isCopied, setIsCopied] = useState(false);
+  const copiedClipboard = async () => {
+    setIsCopied(true);
+    await navigator.clipboard.writeText(text);
+  };
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsCopied(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [isCopied]);
   return (
     <div className='clipboard-container'>
       <div className='clipboard'>
-        <span>npx create-strapi-app@latest my-project </span>
-        <button className='btn clipboard-btn'>
-          <HiOutlineClipboard />
+        <span>{text}</span>
+        <button className='btn clipboard-btn' onClick={copiedClipboard}>
+          {isCopied ? (
+            <TiTick style={{ color: "green" }} />
+          ) : (
+            <HiOutlineClipboard />
+          )}
         </button>
       </div>
       <div className='btn-container'>
